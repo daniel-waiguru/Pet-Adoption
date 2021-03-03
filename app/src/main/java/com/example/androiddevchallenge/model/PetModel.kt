ef791651e9f3c40acb151295b12dfa4c7b8b6f35
Daniel Waiguru <danielirungu91@gmail.com>
2021-03-03 12:54:17 +0300

    add PetModel and image resources

---
+++ app/src/main/java/com/example/androiddevchallenge/model/PetModel.kt
@@ -0,0 +1,11 @@
+package com.example.androiddevchallenge.model
+
+data class PetModel(
+    val id: Int,
+    val name: String,
+    val gender: String,
+    val breed: String,
+    val weight: Double,
+    val age: Int,
+    val imageRes: Int
+)
