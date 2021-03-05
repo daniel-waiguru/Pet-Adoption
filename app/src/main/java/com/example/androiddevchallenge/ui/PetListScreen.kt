82f2b511bd39df1b722b4d5ba083583d5f40608b
Daniel Waiguru <danielirungu91@gmail.com>
2021-03-05 09:53:51 +0300

    Add app TopAppBar

--- app/src/main/java/com/example/androiddevchallenge/ui/PetListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PetListScreen.kt
@@ -2,14 +2,8 @@ package com.example.androiddevchallenge.ui
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.model.PetModel
 import com.example.androiddevchallenge.repository.DataStore
 import com.example.androiddevchallenge.ui.components.DisplayPets
@@ -17,22 +11,12 @@ import com.example.androiddevchallenge.ui.components.DisplayPets
 @Composable
 fun PetListScreen(onPetItemClicked:(PetModel) ->Unit) {
     Column {
-        HeaderText()
+        androidx.compose.material.TopAppBar(title = {
+            Text(text = "Pet Adopter")
+        })
         Log.d("Pets", DataStore.allPets.toString())
         DisplayPets(DataStore.allPets) {
             onPetItemClicked(it)
         }
     }
-}
-
-@Composable
-fun HeaderText() {
-    Text(
-        text = "Adopt A Pet",
-        style = MaterialTheme.typography.h4,
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth(),
-        textAlign = TextAlign.Center
-    )
 }
